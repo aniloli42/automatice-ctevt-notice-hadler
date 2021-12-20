b5c636ef71bd11b9bfe3d0b885939f54a62c1a41
aniloli42 <aniloli42@gmail.com>
2021-12-21 04:51:36 +0545

    fix: array added

--- services/scrapper.js
+++ services/scrapper.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer")
 const scrapper = async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    args: "--no-sandbox",
+    args: ["--no-sandbox"],
   })
 
   const page = await browser.newPage()
