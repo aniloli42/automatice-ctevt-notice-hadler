8f81080247613ef625ee86b722304341e9b7c379
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2023-11-20 12:43:53 +0545

    refactor: headless

--- src/services/scrapper.ts
+++ src/services/scrapper.ts
@@ -8,7 +8,7 @@ const scrapper = async () => {
 
 	try {
 		browser = await puppeteer.launch({
-			headless: false,
+			headless: 'new',
 			args: [
 				'--no-sandbox',
 				'--disable-setuid-sandbox',
