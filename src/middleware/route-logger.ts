c039431fcf44740c05a4c73d2465f12714797942
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2023-11-28 14:16:42 +0545

    feat: cron job created, logger refactored

--- src/middleware/route-logger.ts
+++ src/middleware/route-logger.ts
@@ -1,7 +1,7 @@
-import { type Request } from 'express';
+import type { Request, Response } from 'express';
 import logger from '../services/logger.js';
 
-export const calledRouteLogger = (req: Request, res, next) => {
+export const calledRouteLogger = (req: Request, res: Response, next) => {
 	const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
 	logger.info(`${req.headers['user-agent']} ${req.originalUrl} from ${ip}`);
 	next();
--- src/notices/notice.route.ts
+++ src/notices/notice.route.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import { noticeCacheMiddleware } from '../middleware/notice-cache.middleware.js';
-import { checkNotice, getNotice, getNotices } from './notice.controller.js';
+import { getNotice, getNotices } from './notice.controller.js';
 
 const router = Router();
-
-router.route('/notice/check').get(checkNotice);
 
 router.route('/v1/api/notices').get(noticeCacheMiddleware, getNotices);
 router.route('/v1/api/notices/:id').get(noticeCacheMiddleware, getNotice);
