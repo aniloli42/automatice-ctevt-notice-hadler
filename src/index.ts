e32f393f8f4e8a2a468ab25a851f4dfd6896d1d1
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2024-03-26 15:27:37 +0545

    refactor: client route logging middleware

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import connectMongoDB from '@common/config/db.js'
 import { config } from '@common/config/env.js'
 import {
-	LIMIT_INTERVAL,
-	NO_OF_REQUESTS
+  LIMIT_INTERVAL,
+  NO_OF_REQUESTS
 } from '@common/constants/app.constants.js'
 import { GREET } from '@common/constants/greet.constants'
 import { gracefulShutdown } from '@common/utils/gracefulShutdown'
-import { calledRouteLogger } from '@middleware/route-logger.js'
+import { logRouteRequestClient } from '@middleware/route-logger.js'
 import { runNoticeCheck } from '@notices/notice.cron-job.js'
 import noticeRoutes from '@notices/notice.route.js'
 import logger from '@services/logger.js'
@@ -19,18 +19,18 @@ import helmet from 'helmet'
 
 const app = express()
 const corsOptions: CorsOptions = {
-	methods: 'GET',
-	origin: '*'
+  methods: 'GET',
+  origin: '*'
 }
 
 const rateLimiter = rateLimit({
-	windowMs: LIMIT_INTERVAL,
-	limit: NO_OF_REQUESTS,
-	legacyHeaders: false,
-	standardHeaders: true,
-	validate: {
-		default: true
-	}
+  windowMs: LIMIT_INTERVAL,
+  limit: NO_OF_REQUESTS,
+  legacyHeaders: false,
+  standardHeaders: true,
+  validate: {
+    default: true
+  }
 })
 
 app.set('trust proxy', config.TRUST_PROXY_LEVEL)
@@ -38,7 +38,7 @@ app.use(cors(corsOptions))
 app.use(rateLimiter)
 app.use(helmet())
 app.use(compression())
-app.use(calledRouteLogger)
+app.use(logRouteRequestClient)
 
 gracefulShutdown()
 await connectMongoDB()
@@ -46,12 +46,12 @@ await connectMongoDB()
 if (process.env.NODE_ENV !== 'production') runNoticeCheck.start()
 
 const routeGuides = (_: Request, res: Response) => {
-	res.json(GREET)
+  res.json(GREET)
 }
 
 app.get('/', routeGuides)
 app.use(noticeRoutes)
 
 const runningServerLog = () =>
-	logger.info(`Server is live on port ${config.PORT}`)
+  logger.info(`Server is live on port ${config.PORT}`)
 app.listen(config.PORT, runningServerLog)
--- src/middleware/route-logger.ts
+++ src/middleware/route-logger.ts
@@ -1,7 +1,7 @@
-import type { Request, Response } from 'express'
+import type { NextFunction, Request, Response } from 'express'
 import logger from '../services/logger.js'
 
-export const calledRouteLogger = (req: Request, res: Response, next) => {
+export const logRouteRequestClient = (req: Request, _res: Response, next: NextFunction) => {
 	const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
 	logger.info(`${req.headers['user-agent']} ${req.originalUrl} from ${ip}`)
 	next()
